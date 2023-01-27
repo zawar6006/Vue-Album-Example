c5dc55f4fdcf8a5f9d854369d64fc00d16136596
Rattikon <zawar6006@hotmail.com>
2023-01-27 16:42:03 +0700

    ver4

--- src/components/LikeorSad.vue
+++ src/components/LikeorSad.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="btn-group">
     <button type="button" class="h1btn-sm btn-outline-success" @click="ilike">
-      <img src="emoji-smile-fill.svg" alt="Like" width="24" height="24" />
+      <!-- <img src="emoji-smile-fill.svg" alt="Like" width="24" height="24" /> -->
       {{ likes }}
       Likes
     </button>
     <button type="button" class="h1btn-sm btn-outline-danger" @click="isad">
-      <img src="emoji-frown-fill.svg" alt="Sad" width="24" height="24" />
+      <!-- <img src="emoji-frown-fill.svg" alt="Sad" width="24" height="24" /> -->
       {{ sads }}
       Sad
     </button>
